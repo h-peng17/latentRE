a329c16bb8678f47355255dbf6b86c0808621325
h-peng17 <penghao20170136@163.com>
2019-10-29 13:50:19 +0800

    bugfix

--- code/model.py
+++ code/model.py
@@ -37,23 +37,14 @@ class LatentRE(nn.Module):
             ce_loss = self.loss.ce_loss(logit, query)
             kl_loss = self.loss.kl_loss(logit, knowledge)
             if Config.latent:
-                gen_loss, score = self.decoder(input_ids, attention_mask, mask, latent)
-                return kl_loss + gen_loss * Config.gen_loss_scale + ce_loss * Config.ce_loss_scale, score
+                gen_loss = self.decoder(input_ids, attention_mask, mask, latent)
+                return kl_loss + gen_loss * Config.gen_loss_scale + ce_loss * Config.ce_loss_scale
             else:
                 return kl_loss * Config.kl_loss_scale + ce_loss * Config.ce_loss_scale
         else:
             text = self.encoder(input_ids, attention_mask)
             logit = self.selector(text, scope)
             return logit
-        # if Config.training:
-        #     text = self.textRepre(word, pos1, pos2)
-        #     logit, _ = self.selector(text, scope, query)
-        #     ce_loss = self.loss.ce_loss(logit, label)
-        #     return ce_loss
-        # else:
-        #     text = self.textRepre(word, pos1, pos2)
-        #     logit = self.selector(text, None)
-        #     return logit
 
 
 
