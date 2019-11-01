f7708c948357db6a33166566e4456136a7d3cf22
h-peng17 <penghao20170136@163.com>
2019-11-01 14:41:44 +0800

    bug fix

--- code/model.py
+++ code/model.py
@@ -42,7 +42,8 @@ class LatentRE(nn.Module):
             if Config.latent:
                 gen_loss = self.decoder(decoder_input_ids, decoder_attention_mask, mask, latent)
                 margin_gen_loss = self.decoder_margin(decoder_input_ids, decoder_attention_mask, mask, None)
-                return torch.max(kl_loss + gen_loss * Config.gen_loss_scale + ce_loss * Config.ce_loss_scale - margin_gen_loss + 2.0, 0)
+                loss, _ = torch.max(kl_loss + gen_loss * Config.gen_loss_scale + ce_loss * Config.ce_loss_scale - margin_gen_loss + 2.0, torch.zeros(0, dtype=torch.float32))
+                return loss
             else:
                 return kl_loss * Config.kl_loss_scale + ce_loss * Config.ce_loss_scale
         else:
