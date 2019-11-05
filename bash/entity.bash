860b10cec0e1b05ad13e176883c3f46a8e87a83a
Tianyu Gao <gaotianyu1350@126.com>
2019-11-05 09:12:30 -0500

    reslt

--- bash/entity.bash
+++ bash/entity.bash
@@ -5,10 +5,9 @@ uid=$RANDOM
 echo $uid
 python main.py --cuda "0,1,2,3" \
     --batch_size 200 \
-    --gen_loss_scale 0.1 \
-    --ce_loss_scale 0 \
+    --gen_loss_scale 0.05 \
     --lr 5e-5 \
-    --info 01gen0ceentitygpt \
+    --info 005genceentity \
     --latent \
     --mask_mode entity \
     --max_epoch 3
