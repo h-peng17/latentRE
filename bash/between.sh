1a16dfcad8c3712a97d2b30d3adb2f9b483c7cb6
Tianyu Gao <gaotianyu1350@126.com>
2019-11-05 03:57:40 -0500

    result

--- bash/between.sh
+++ bash/between.sh
@@ -4,11 +4,11 @@ module load cuda/10.0.130
 uid=$RANDOM
 echo $uid
 python main.py --cuda "0,1,2,3" \
-    --batch_size 192 \
+    --batch_size 144 \
     --gen_loss_scale 0.1 \
-    --ce_loss_scale 0.5 \
+    --ce_loss_scale 0 \
     --lr 5e-5 \
-    --info 01gen05cebetweenmargin \
+    --info 01gen0cebetweengpt2 \
     --latent \
     --mask_mode between \
     --max_epoch 3
--- bash/entity.bash
+++ bash/entity.bash
@@ -4,11 +4,11 @@ module load cuda/10.0.130
 uid=$RANDOM
 echo $uid
 python main.py --cuda "0,1,2,3" \
-    --batch_size 192 \
-    --gen_loss_scale 1 \
-    --ce_loss_scale 0.5 \
+    --batch_size 200 \
+    --gen_loss_scale 0.1 \
+    --ce_loss_scale 0 \
     --lr 5e-5 \
-    --info 1gen05ceentity \
+    --info 01gen0ceentitygpt \
     --latent \
     --mask_mode entity \
     --max_epoch 3
