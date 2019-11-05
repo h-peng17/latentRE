b745b290e3fca694ef166bd066a07325b92fe8ca
h-peng17 <penghao20170136@163.com>
2019-11-05 20:05:48 +0800

    stable version

--- code/model.py
+++ code/model.py
@@ -43,7 +43,7 @@ class LatentRE(nn.Module):
             kl_loss = self.loss.kl_loss(logit, knowledge)
             if Config.latent:
                 gen_loss = self.decoder(decoder_input_ids, decoder_attention_mask, mask, latent)
-                return kl_loss + gen_loss * Config.gen_loss_scale
+                return ce_loss + gen_loss * Config.gen_loss_scale # !!!!!!
             else:
                 return kl_loss * Config.kl_loss_scale + ce_loss * Config.ce_loss_scale
         else:
