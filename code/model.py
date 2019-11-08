444ba94b786eed0cb8793e249f793382266c98fc
h-peng17 <penghao20170136@163.com>
2019-11-08 22:42:57 +0800

    traindecoder

--- code/model.py
+++ code/model.py
@@ -20,11 +20,11 @@ class LatentRE(nn.Module):
     def __init__(self, word_vec, weight=None):
         super(LatentRE, self).__init__()
         ''' load encoder '''
-        # checkpoint = torch.load(os.path.join(Config.save_path, "ckptceentity1"))
+        checkpoint = torch.load(os.path.join(Config.save_path, "ckptceentity1"))
         self.encoder = Bert()
-        # self.encoder.load_state_dict(checkpoint["model"])
-        # for param in self.encoder.parameters():
-            # param.requires_grad = False # frozen
+        self.encoder.load_state_dict(checkpoint["model"])
+        for param in self.encoder.parameters():
+            param.requires_grad = False # frozen
         self.selector = Selector()
         self.decoder = BertDecoder()
         self.loss = Loss(weight)
