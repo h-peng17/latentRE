33b882edf69f273c4f8720be4a6378c2c40d1efa
h-peng17 <penghao20170136@163.com>
2019-11-10 16:35:21 +0800

    maskentiy

--- code/dataloader.py
+++ code/dataloader.py
@@ -225,8 +225,11 @@ class Dataloader:
                     len_sec = len(head_tokens)
                 self.data_between_entity_mask[i][fir_pos+len_fir+1:sec_pos-1] = 1     
                 self.data_token_mask[i][head_pos-1:head_pos+len(head_tokens)+1] = 0
-                self.data_token_mask[i][tail_pos-1:tail_pos+len(tail_tokens)+1] = 0   
-                
+                self.data_token_mask[i][tail_pos-1:tail_pos+len(tail_tokens)+1] = 0
+
+                # mask entity
+                self.data_input_ids[i][head_pos-1:head_pos+len(head_tokens)+1] = bert_tokenizer.convert_tokens_to_ids(bert_tokenizer.mask_token)   
+                self.data_input_ids[i][tail_pos-1:tail_pos+len(tail_tokens)+1] = bert_tokenizer.convert_tokens_to_ids(bert_tokenizer.mask_token)
 
                 # for mask 
                 # words = sentence.split()
