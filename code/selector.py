72b2d1435a90738867c29e50a4d44452b808abc7
h-peng17 <penghao20170136@163.com>
2019-11-19 23:42:02 +0800

    fsd

--- code/selector.py
+++ code/selector.py
@@ -67,7 +67,7 @@ class Selector(nn.Module):
                     return bag_logit
                 elif Config.bag_type == "att":
                     bag_repre = []
-                    att_mat = self.rel_mat.transpose(0,1)[query]
+                    att_mat = self.rel_mat[query]
                     att_score = (x * att_mat).sum(-1)
                     for i in range(scope.shape[0]):
                         bag_hidden_mat = x[scope[i][0]:scope[i][1]] # (bag_size, hidden_size)
