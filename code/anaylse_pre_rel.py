73ad735ad1f0e27929c136fb288b47c2d42a725c
h-peng17 <penghao20170136@163.com>
2019-11-24 13:47:50 +0800

    das

--- code/anaylse_pre_rel.py
+++ code/anaylse_pre_rel.py
@@ -29,19 +29,28 @@ def eval(logit, label, relid):
         # print("auc = "+str(auc)+"| "+"F1 = "+str(f1))
         # print('P@100: {} | P@200: {} | P@300: {} | Mean: {}'.format(precision[100], precision[200], precision[300], (precision[100] + precision[200] + precision[300]) / 3))
         # print("---------------------------------------------------------")
-        log(auc, relid, precision)
+        log(auc, f1, relid, precision)
+        return auc
 
-def log(auc, relid, precision):
+def log(auc, f1, relid, precision):
     if not os.path.exists("../res"):
         os.mkdir("../res")
-    f = open(os.path.join("../res", "bert_log"), 'a+')
+    f = open(os.path.join("../res", "bert+nyt_log"), 'a+')
+    f.write("---------------------------------------------------------")
     f.write("relation id: %d\n" % relid)
     f.write("auc = "+str(auc)+"| "+"F1 = \n"+str(f1))
     f.write('P@100: {} | P@200: {} | P@300: {} | Mean: {}\n'.format(precision[100], precision[200], precision[300], (precision[100] + precision[200] + precision[300]) / 3))
+    f.write("---------------------------------------------------------")
     f.close()
 
 rel2id = json.load(open("../data/nyt/rel2id.json"))
 logit = np.load("../res/bert+nyt_logit.npy")
 label = np.load("../res/pcnn+att+wiki_label.npy")
+result = []
 for i in range(1, len(rel2id)):
-    eval(logit, label, i)
+    _result = []
+    _result.append(i)
+    auc = eval(logit, label, i)
+    _result.append(auc)
+    result.append(_result)
+json.dump(result, open(os.path.join("../res", "bert+nyt_result.json"), 'w'))
